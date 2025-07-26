a922417b71c607554b694f4c6804d3e1875e09a8
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-07-27 02:57:56 +0530

    fix: update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        popup: resolve("index.html"),
+        content: resolve("src/lib/content.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
